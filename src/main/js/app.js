ad0b8309c0e6958b8ac3424b556f7521dfd954a8
Grady Arnold <gradyarnold@pop-os.localdomain>
2022-02-15 20:09:59 -0500

    Frontend files updated

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -2,3 +2,40 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const client = require('./client');
 
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {stuff: []}
+    }
+
+    componentDidMount() {
+
+        client({method: 'GET', path: '/twitter/search'}).done(response => {
+            this.setState({stuff: response})
+        });
+    }
+
+    render() {
+        return (
+            <OutputTest ouput = OutputTest/>
+        )
+    }
+}
+
+class OutputTest extends React.Component{
+    render() {
+
+        return (
+            <div>
+                hello world
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('react')
+)
+// end::render[]
