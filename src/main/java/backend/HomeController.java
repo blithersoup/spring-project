446eee4f22c879ebd61ff9893b0cc8197da6f783
blithersoup <gradyarnold11@gmail.com>
2022-02-22 15:12:30 -0500

    Working full stack server

--- src/main/java/backend/HomeController.java
+++ src/main/java/backend/HomeController.java
@@ -1,15 +1,14 @@
 package backend;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
-    @GetMapping (value = "/grady")
+
+    @GetMapping (value = "/")
     public String base() {
-        return "grady";
+        return "index.html";
     }
+
 }
--- src/main/resources/static/index.jsp
+++
@@ -1,8 +0,0 @@
-<%@ page language="html" contentType="application/javascript; charset=UTF-8" pageEncoding="UTF-8"%>
-<html>
-    <head>
-        <title>Welcome</title>
-    </head>
-
-    <script type="text/javascript" src="js/frontend/src/index.js"></script>
-</html>
